3553847f25e837651c834b77265ab4db7aa3f523
sergiogyoz <sergiogyoz@hotmail.com>
2021-08-25 01:26:06 -0500

    quick ARMA for future testing

--- Main.py
+++ Main.py
@@ -3,15 +3,23 @@ import watlevpy.time_series as wal #base class for time series
 #for plotting
 import watlevpy.plot.year_evol as yevol
 import watlevpy.plot.simple as wplot
+import matplotlib.pyplot as plt
 #datetime default library
 import datetime
 #for stats testing
-from scipy import stats 
+from scipy import stats
+#for ARIMA and autocorrelations
+import statsmodels.graphics.tsaplots as smgt
+import statsmodels.tsa.arima_model as smta
 
-#WL=wal.TSReader.from_csvfile(csvfile="./DubuqueIA.csv",headers=True,dateformat="%m/%d/%Y %H:%M");
+WL=wal.TSReader.from_csvfile(csvfile="./DubuqueIA.csv",headers=True,dateformat="%m/%d/%Y %H:%M");
 #foryearplot=wal.TS.sub_wl(WL, datetime.date(WL.first_date.year+1,1,1), datetime.date(WL.last_date.year-1,12,31));
+foryearplot=wal.TS.sub_wl(WL, datetime.date(1950,1,1), datetime.date(WL.last_date.year-1,12,31));
 
-#yaver=wal.TSFilter.averages_from_TS(WL, "yearly");
+yaver=wal.TSFilter.averages_from_TS(foryearplot, "yearly");
+wal.TS.save_to_csv(yaver,"myyeardata");
+
+
 #wplot.plot(yaver,gtype=2);
 
 #ypeaks=wal.TSFilter.peaks_from_TS(WL, "monthly");
@@ -28,40 +36,29 @@ from scipy import stats
 
 #-----Let's do some testing
 
-#Kendall testing
-kdata=wal.TSReader.from_csvfile(csvfile="./KendalldataNotrend.csv",headers=True,dateformat="%m/%d/%Y %H:%M");    
-#kdata=wal.TSReader.from_csvfile(csvfile="./KendalldataTrend.csv",headers=True,dateformat="%m/%d/%Y %H:%M");    
+def autocorr(x, maxlag=10):
+    aut=[];
+    m=len(x)-maxlag;
+    for t in range(maxlag+1):
+        aut.append(np.corrcoef(x[0:m], x[t:m+t])[0][1]);
+    return aut;
 
-t=list(range(kdata.n));
-tau, p_val= stats.kendalltau(t, kdata.wl);
 
-n=kdata.n;
-s=0;
-for i in range(0,n-1):
-    for j in range(i+1,n):
-        s=s+np.sign(kdata.wl[j]-kdata.wl[i]);
+plt.figure();
+rho=autocorr(yaver.wl,maxlag=30);
+plt.plot(rho);
 
-tp=[0]*20;
-for val in kdata.wl:
-    tp[int(val)]=tp[int(val)]+1;
-    
-var=n*(n-1)*(2*n+5);
-for t in tp:
-    var=var-t*(t-1)*(2*t+5);
-var=var/18;
+smgt.plot_acf(yaver.wl,lags=30);
+smgt.plot_pacf(yaver.wl,lags=30);
 
-#Z= (s-1)/np.sqrt(var) if s>0 else (s+1)/np.sqrt(var);
-Z=s/np.sqrt(var);
-
-myp_val=2*stats.norm.cdf(Z);
+arimastuff=smta.ARIMA(yaver.wl, order=(1,0,0));
+arimafit=arimastuff.fit(disp=0)
+print(arimafit.summary())
 
 
 
-
-
-
-
-
+arimafit.plot_predict(dynamic=False);
+plt.show();
 
 
 
